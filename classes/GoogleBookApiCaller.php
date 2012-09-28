fec81e93702551249231daabaee382822340b43f
TheLRBH <bibouh123@live.fr>
2012-09-28 17:39:40 +0200

    resolution de PB git

--- classes/GoogleBookApiCaller.php
+++ classes/GoogleBookApiCaller.php
@@ -14,26 +14,30 @@ class GoogleBookApiCaller {
 
     //URL of google book api
     var $key = "AIzaSyBaZwSa7tSjJnWmiCGYnEY087u-P-aGFGE";
-    var $url = "https://www.googleapis.com/books/v1/volumes?q=";
+    var $url = "http://www.googleapis.com/books/v1/volumes?q=";
 
     //put your code here
     function callAuthor($author) {
-        $url_ = "https://www.googleapis.com/books/v1/volumes?q=yasmina&key=AIzaSyBaZwSa7tSjJnWmiCGYnEY087u-P-aGFGE";
+        $url_ = "https://www.googleapis.com/books/v1/volumes?q=yasmina";
 
         //DEBUG
         var_dump($url_);
-
+        
 //Curl Requete
         $c = curl_init();
         curl_setopt($c, CURLOPT_URL, $url_);
         curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($c, CURLOPT_HEADER, false);
         curl_setopt($c, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
         $resultat = curl_exec($c);
 
         //DEBUG
         echo "<br/>";
         var_dump($resultat); // */
+        echo "<br/>";
+        var_dump(curl_error($c));
+
     }
 
 }
