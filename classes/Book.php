c55b340ff801af8c059adf9cb5484932fe2fe0ca
LRBH10 <rabah.laouadi@gmail.com>
2012-09-28 13:04:05 +0000

    add netbeans projects

---
+++ classes/Book.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Book
+ *
+ * @author bibouh
+ */
+class Book {
+    //put your code here
+}
+
+?>
