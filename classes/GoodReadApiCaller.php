013c763e3ddd9c9c11507d9559473d40e37f57b1
TheLRBH <bibouh123@live.fr>
2012-10-18 19:42:30 +0200

    adding ApiCaller GoodReadApiCaller

---
+++ classes/GoodReadApiCaller.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+include_once 'ApiCaller.php';
+
+/**
+ * Description of GoodReadApiCaller
+ *
+ * @author Rabah
+ */
+class GoodReadApiCaller {
+
+    //put your code here
+    var $key = "IJ9e4JY4Gnl1JU5ADSYYg";
+    var $url_search = "http://www.goodreads.com/search.xml";
+    var $url_author = "http://www.goodreads.com/author/show/";
+
+    public function searchAuthor($author) {
+        
+    }
+
+}
+
+?>
--- classes/GoogleBookApiCaller.php
+++ classes/GoogleBookApiCaller.php
@@ -4,37 +4,30 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+include_once 'ApiCaller.php';
 include_once 'Book.php';
+
 /**
  * Description of GoogleBookApiCaller
  *
  * @author bibouh
  */
-
-class GoogleBookApiCaller {
+class GoogleBookApiCaller extends ApiCaller {
 
     //URL of google book api
     var $key = "AIzaSyBaZwSa7tSjJnWmiCGYnEY087u-P-aGFGE";
     var $url = "https://www.googleapis.com/books/v1/volumes?maxResults=5&prettyPrint=false&printType=books&orderBy=relevance&q=";
 
     //put your code here
-    function callAuthor($author, $lang = 'fr') {
-
+    function callAuthor($author) {
         $author_ = urlencode($author);
-        $url_ = $this->url . "inauthor:" . $author_; //. "&langRestrict=" . $lang;
+        $url_ = $this->url . "inauthor:" . $author_;//. "&langRestrict=" . $lang;
         //DEBUG
         var_dump($url_);
         echo "<br/>";
 
 //Curl Requete
-        $c = curl_init();
-        curl_setopt($c, CURLOPT_URL, $url_);
-        curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($c, CURLOPT_HEADER, false);
-        curl_setopt($c, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
-        $resultat = curl_exec($c);
-
+        $resultat = $this->callApi($url_, true);
         $json_result = json_decode($resultat, true);
 
         return $this->generateBooksOfAuthor($json_result);
@@ -47,7 +40,6 @@ class GoogleBookApiCaller {
                 //DEBUG
                 echo "<br/>****************<br/><br/>";
                 $list_of_books[] = Book::parseFromJson($book);
-                //var_dump(Book::parseFromJson($book));
             }
         } else {
             echo "<br/> Fuck requete";
