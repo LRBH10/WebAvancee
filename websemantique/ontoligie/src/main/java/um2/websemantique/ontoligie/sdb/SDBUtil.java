9247f256aaaec469e8e8fd5a6a2c797b848ff7a1
LRBH10 <bibouh123@live.fr>
2012-10-31 08:05:19 +0100

    creation of jena Base

--- websemantique/ontoligie/src/main/java/um2/websemantique/ontoligie/sdb/SDBUtil.java
+++ websemantique/ontoligie/src/main/java/um2/websemantique/ontoligie/sdb/SDBUtil.java
@@ -1,11 +1,9 @@
 package um2.websemantique.ontoligie.sdb;
 
-import com.hp.hpl.jena.sdb.StoreDesc;
+import com.hp.hpl.jena.db.DBConnection;
+import com.hp.hpl.jena.db.IDBConnection;
 import com.hp.hpl.jena.sdb.sql.JDBC;
 import com.hp.hpl.jena.sdb.sql.SDBConnection;
-import com.hp.hpl.jena.sdb.store.DatabaseType;
-import com.hp.hpl.jena.sdb.store.LayoutType;
-
 
 public class SDBUtil {
 
@@ -15,13 +13,16 @@ public class SDBUtil {
 		String jdbcURL = "jdbc:mysql://localhost:3306/rdf_base";
 		SDBConnection conn = new SDBConnection(jdbcURL, "root", "rabah123");
 		return conn;
-
 	}
 
-	public static StoreDesc getStoreDesc() {
-		StoreDesc storeDesc = new StoreDesc(LayoutType.LayoutTripleNodesHash,
-				DatabaseType.MySQL);
-		return storeDesc;
-	}
+	public static IDBConnection getIDBConnection() {
 
+		JDBC.loadDriverMySQL();
+		String jdbcURL = "jdbc:mysql://localhost:3306/rdf_base";
+
+		IDBConnection conn = new DBConnection(jdbcURL, "root", "rabah123",
+				"MySQL");
+
+		return conn;
+	}
 }
--- websemantique/ontoligie/src/test/java/Main.java
+++ websemantique/ontoligie/src/test/java/Main.java
@@ -1,3 +1,14 @@
+import um2.websemantique.ontoligie.sdb.SDBUtil;
+
+import com.hp.hpl.jena.db.DBConnection;
+import com.hp.hpl.jena.db.IDBConnection;
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.ontology.OntModelSpec;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.ModelMaker;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.Resource;
 
 public class Main {
 
@@ -5,7 +16,43 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-	
-	}
+		try {
 
+			IDBConnection conn = SDBUtil.getIDBConnection();
+
+			ModelMaker maker = ModelFactory.createModelRDBMaker(conn);
+			Model tmp = null;
+			if (conn.containsModel("Maladie")) {
+				System.out.println("Opening existing model");
+				tmp = maker.openModel("Maladie", true);
+			} else {
+				System.out.println("Creating new model");
+				tmp = maker.createModel("Maladie", true);
+			}
+			// Start a database transaction. Without one, each statement will be
+			// auto-committed
+			// as it is added, which slows down the model import significantly.
+			// tmp.begin();
+			OntModelSpec spec = new OntModelSpec(OntModelSpec.OWL_DL_MEM);
+			OntModel mdb = ModelFactory.createOntologyModel(spec, tmp);
+			String ns = "http://t.t.fr/";
+			mdb.setNsPrefix("Maladie", ns);
+			Resource m = mdb.createClass(ns + "Grippe");
+			Resource v = mdb.createClass(ns + "FLUAV");
+			Property p = mdb.createObjectProperty(ns + "AprOrigine");
+
+			tmp.add(m, p, v);
+
+			// Commit the database transaction
+			// tmp.commit();
+
+			System.out.println("Contenu " + tmp.size());
+			tmp.write(System.out, "RDF/XML-ABBREV");
+			// Close the database connection
+			conn.close();
+		} catch (Exception e) {
+			System.out.println("Exception");
+			System.out.println(e);
+		}
+	}
 }
