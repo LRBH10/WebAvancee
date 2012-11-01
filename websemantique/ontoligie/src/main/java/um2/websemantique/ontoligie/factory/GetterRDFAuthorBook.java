a7631366be910e080883812d36f0c91477d3638f
LRBH10 <bibouh123@live.fr>
2012-11-01 14:48:38 +0100

    add CLASS GetterRDF

---
+++ websemantique/ontoligie/src/main/java/um2/websemantique/ontoligie/factory/GetterRDFAuthorBook.java
@@ -0,0 +1,10 @@
+package um2.websemantique.ontoligie.factory;
+
+import um2.websemantique.entities.utils.SearchType;
+
+public class GetterRDFAuthorBook {
+	private String key;
+	private SearchType type;
+	
+	
+}
