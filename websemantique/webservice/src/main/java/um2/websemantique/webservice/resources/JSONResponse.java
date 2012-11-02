c1090e8214c1cc7dc49841ade9baf89f842c50d7
LRBH10 <bibouh123@live.fr>
2012-11-02 13:33:46 +0100

    Web Service in Use

--- websemantique/webservice/src/main/java/um2/websemantique/webservice/resources/JSONResponse.java
+++ websemantique/webservice/src/main/java/um2/websemantique/webservice/resources/JSONResponse.java
@@ -4,12 +4,22 @@ import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
-@Path("/tests/")
-@Produces("application/json")
-public class OperationTestResources {
+@Path("/")
+@Produces(MediaType.APPLICATION_JSON)
+public class JSONResponse {
 
+	@GET
+	@Path("excuteSPRQL/{query}")
+	public String excuteSPRQL(@PathParam("query") String query) {
+		String res =  query;
+		return res + "\n";
+	}
+
+	
 	@GET
 	@Path("testGet")
 	public String testGET() {
